ba4e84bc018a41b2d114f324d7cc1ba0b345a835
Aryan Bhalla <aryanbhalla66@gmail.com>
2021-04-19 14:24:03 -0500

    input location data done

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,10 @@ const baseURL = `https://api.mapbox.com/geocoding/v5/`;
 const apiKEY = `?access_token=pk.eyJ1IjoiYXJ5YW5iaGFsbGEiLCJhIjoiY2ttbWMxYjN0MG4zNzJ2b2RzenNtNHloeCJ9.D28HxdUCUpf7YpvsQZ26AQ`;
 const originsDOM = document.querySelector('.origins');
 const destinationDOM = document.querySelector('.destinations');
+const tripButton = document.querySelector('.plan-trip');
+
+let origin = ``;
+let destination = ``;
 
 originForm.addEventListener('keydown', e => {
   const inputBar = document.querySelector('.origin-form input');
@@ -39,14 +43,32 @@ async function getLocation(query, location) {
 function drawLocationDOM(locations, outputLocation) {
   outputLocation.textContent = ``;
   locations.forEach(singleLocation => {
-    let name = singleLocation.place_name.split(',')[0];
-    let location = singleLocation.place_name.split(',')[1];
     outputLocation.insertAdjacentHTML('beforeend', `
-      <li data-long="-97.113936" data-lat="49.823059">
-        <div class="name">${name}</div>
-        <div>${location}</div>
+      <li data-long="${singleLocation.center[0]}" data-lat="${singleLocation.center[1]}">
+        <div class="name">${singleLocation.place_name.split(',')[0]}</div>
+        <div>${singleLocation.place_name.split(',')[1]}</div>
       </li>
     `);
   })
 }
+
+originsDOM.addEventListener('click', e => {
+  let long = e.target.closest('LI').dataset.long;
+  let lat = e.target.closest('LI').dataset.lat;
+  origin = `${long},${lat}`;
+  console.log(origin);
+});
+
+destinationDOM.addEventListener('click', e => {
+  let long = e.target.closest('LI').dataset.long;
+  let lat = e.target.closest('LI').dataset.lat;
+  destination = `${long},${lat}`;
+  console.log(destination);
+});
+
+tripButton.addEventListener('click', e => {
+  if(destination !== `` && origin !== ``){
+    
+  }
+});
 
