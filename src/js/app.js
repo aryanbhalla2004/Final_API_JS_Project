f705d7025a6487957e1bde29a2f8992fbc62a734
Aryan Bhalla <aryanbhalla66@gmail.com>
2021-04-20 14:36:26 -0500

    finsihed trip plans

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ const apiKEY = `?access_token=pk.eyJ1IjoiYXJ5YW5iaGFsbGEiLCJhIjoiY2ttbWMxYjN0MG4
 const originsDOM = document.querySelector('.origins');
 const destinationDOM = document.querySelector('.destinations');
 const tripButton = document.querySelector('.plan-trip');
+const routeDOM = document.querySelector('.my-trip');
 
 let origin = ``;
 let destination = ``;
@@ -14,7 +15,6 @@ originForm.addEventListener('keydown', e => {
   if(e.which === 13){
     e.preventDefault();
     getLocation(inputBar.value, 'origin');
-    inputBar.value = ``;
   }
 });
 
@@ -23,7 +23,6 @@ destinationForm.addEventListener('keydown', e => {
   if(e.which === 13){
     e.preventDefault();
     getLocation(inputBar.value, 'destination');
-    inputBar.value = ``;
   }
 })
 
@@ -32,43 +31,90 @@ async function getLocation(query, location) {
   if(location === 'origin'){
     const response = await fetch(`${baseURL}mapbox.places/${query}.json${apiKEY}&limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275`);
     const JSON = await response.json();
-    drawLocationDOM(JSON.features, originsDOM);
+    drawLocationDOM(JSON.features, originsDOM, false);
   } else if(location === 'destination'){
     const response = await fetch(`${baseURL}mapbox.places/${query}.json${apiKEY}&limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275`);
     const JSON = await response.json();
-    drawLocationDOM(JSON.features, destinationDOM);
+    drawLocationDOM(JSON.features, destinationDOM, false);
   }
 }
 
-function drawLocationDOM(locations, outputLocation) {
-  outputLocation.textContent = ``;
-  locations.forEach(singleLocation => {
+function drawLocationDOM(locations, outputLocation, route) {
+  if(!route) {
+    outputLocation.textContent = ``;
+    locations.forEach(singleLocation => {
+      outputLocation.insertAdjacentHTML('beforeend', `
+        <li data-long="${singleLocation.center[0]}" data-lat="${singleLocation.center[1]}">
+          <div class="name">${singleLocation.place_name.split(',')[0]}</div>
+          <div>${singleLocation.place_name.split(',')[1]}</div>
+        </li>
+      `);
+    });
+  } else {
+    routeDOM.textContent = ``;
     outputLocation.insertAdjacentHTML('beforeend', `
-      <li data-long="${singleLocation.center[0]}" data-lat="${singleLocation.center[1]}">
-        <div class="name">${singleLocation.place_name.split(',')[0]}</div>
-        <div>${singleLocation.place_name.split(',')[1]}</div>
-      </li>
+      <li><span class="material-icons">exit_to_app</span> Depart at ${new Date(locations.segments[0].times.start).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: true, second:'2-digit'})}</li>
     `);
-  })
+
+    locations.segments.forEach(element => {
+      if(element.type === 'walk'){
+        if(element.to === undefined || element.to.destination !== undefined){
+          outputLocation.insertAdjacentHTML('beforeend', `
+            <li><span class="material-icons">directions_walk</span>Walk for ${element.times.durations.total} minutes to your destination, arriving at ${new Date(element.times.end).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: true, second:'2-digit'})}</li>
+          `);
+        } else {
+          outputLocation.insertAdjacentHTML('beforeend', `
+            <li><span class="material-icons">directions_walk</span>Walk for ${element.times.durations.total} minutes to stop #${element.to.stop.key} - ${element.to.stop.name}</li>
+          `);
+        }
+      } else if(element.type === 'ride'){
+        outputLocation.insertAdjacentHTML('beforeend', `
+          <li><span class="material-icons">directions_bus</span>Ride the ${element.route.name} for ${element.times.durations.total} minutes.</li>
+        `);
+      } else if(element.type === 'transfer'){
+        outputLocation.insertAdjacentHTML('beforeend', `
+          <li><span class="material-icons">transfer_within_a_station</span>Transfer from stop #10643 - Northbound Fort atGraham to stop #10611 - Eastbound Graham at Fort (Wpg Square)</li>
+        `);
+      }
+    });
+  }
 }
 
 originsDOM.addEventListener('click', e => {
   let long = e.target.closest('LI').dataset.long;
   let lat = e.target.closest('LI').dataset.lat;
-  origin = `${long},${lat}`;
+  origin = `${lat},${long}`;
   console.log(origin);
 });
 
 destinationDOM.addEventListener('click', e => {
   let long = e.target.closest('LI').dataset.long;
   let lat = e.target.closest('LI').dataset.lat;
-  destination = `${long},${lat}`;
+  destination = `${lat},${long}`;
   console.log(destination);
 });
 
 tripButton.addEventListener('click', e => {
   if(destination !== `` && origin !== ``){
-    
+    getTripData();
   }
 });
 
+
+async function getTripData(){
+  const response = await fetch(`https://api.winnipegtransit.com/v3/trip-planner.json?origin=geo/${origin}&api-key=eg1G3N2HfyiROxEYBM_5&destination=geo/${destination}`);
+  const JSON = await response.json();
+  const lowestTime = JSON.plans.reduce((prev,current) => (new Date(prev.times.end).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'}) < new Date(current.times.end).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'})) ? prev : current);
+  const possibleTimes = [];
+  let time = new Date(lowestTime.times.end).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false });
+  console.log(time);
+  JSON.plans.forEach(element => {
+    if(time >= new Date(element.times.end).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false })){
+      time = new Date(element.times.end).toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit', hour12: false });
+      possibleTimes.push(element);
+    }
+  });
+
+  const outCome = possibleTimes.reduce((prev, current) => (prev.times.durations.total) > (current.times.durations.total) ? prev : current);
+  drawLocationDOM(outCome, routeDOM, true);
+}
